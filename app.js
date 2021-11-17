99967226b428b7e80885e27d18eda43e69466038
EdDonassi <87482442+EdDonassi@users.noreply.github.com>
2021-11-17 23:23:27 +0100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-let navLinks = document.querySelector('.links');
-let menuIcon = document.querySelector('#menu-icon');
-const boxi = document.querySelector('.box');
-
-menuIcon.addEventListener('click', function () {
-    navLinks.classList.toggle('is-active');
-})
